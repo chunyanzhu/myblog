dfe72faf6699f8b8a2a5b39e5297ad87222eb3e6
chunyanzhu <215098516@qq.com>
2016-11-21 11:01:44 +0800

    update

--- libs/db.js
+++ libs/db.js
@@ -2,8 +2,8 @@ var mongo = require('mongodb');
 var host = 'localhost';
 var port = 27017;
 var server  = new mongo.Server(host, port, {auto_reconnect: true});
-//var db = new mongo.Db('zhu_data', server, {safe: true});
 var db = new mongo.Db('zhu_data', server, {safe: true});
+//var db = new mongo.Db('myblogDb', server, {safe: true});
 var writeErrorLog = require('./writeErrorLog');
 console.log('openCalled:' + mongo.openCalled);
 db.on('close', function (err, db) {
